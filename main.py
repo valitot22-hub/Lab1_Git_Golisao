f559dceca6e5c9796fcdd59f2eaa31df84ee95ce
Jocel Jhon Aquino Golisao <valitot22@gmail.com>
2026-02-09 17:11:01 +0800

    Enable login for Golisao branch

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Lab 1: Version Control")
+print("Author: JOCEL JHON A. GOLISAO")
+print("Student ID: TUPM-25-0803")
+print("Lab 1: Version Control")
+print("Author: JOCEL JHON A. GOLISAO")
+print("Student ID: TUPM-25-0803")
+print("Class Section: BSME1B") #
+print("Login Feature Enabled")
